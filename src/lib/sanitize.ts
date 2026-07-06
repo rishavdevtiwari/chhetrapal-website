e324c5c0c5d30721e55c1a65762ef44e4ce083db
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2026-07-06 23:41:16 +0545

    fix broken deployment issue

--- src/lib/sanitize.ts
+++ src/lib/sanitize.ts
@@ -1,4 +1,4 @@
-import DOMPurify from "isomorphic-dompurify";
+import { filterXSS } from "xss";
 
 /**
  * Sanitizes HTML input from the WordPress CMS before rendering to the DOM.
@@ -6,39 +6,29 @@ import DOMPurify from "isomorphic-dompurify";
  */
 export function sanitizeHtml(html: string | undefined): string {
   if (!html) return "";
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: [
-      "p",
-      "br",
-      "strong",
-      "em",
-      "u",
-      "ol",
-      "ul",
-      "li",
-      "a",
-      "img",
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "span",
-      "div",
-      "blockquote",
-      "pre",
-      "code",
-    ],
-    ALLOWED_ATTR: [
-      "href",
-      "src",
-      "alt",
-      "title",
-      "class",
-      "target",
-      "rel",
-      "style",
-    ],
+  return filterXSS(html, {
+    whiteList: {
+      p: ["class", "style"],
+      br: [],
+      strong: [],
+      em: [],
+      u: [],
+      ol: ["class", "style"],
+      ul: ["class", "style"],
+      li: ["class", "style"],
+      a: ["href", "title", "target", "rel", "class", "style"],
+      img: ["src", "alt", "title", "class", "style"],
+      h1: ["class", "style"],
+      h2: ["class", "style"],
+      h3: ["class", "style"],
+      h4: ["class", "style"],
+      h5: ["class", "style"],
+      h6: ["class", "style"],
+      span: ["class", "style"],
+      div: ["class", "style"],
+      blockquote: ["class", "style"],
+      pre: ["class", "style"],
+      code: ["class", "style"],
+    },
   });
 }
